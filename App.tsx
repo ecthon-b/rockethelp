3ffff23ef5e22990375097d11f774abcd6b20f84
ecthon <ecthon@gmail.com>
2022-07-22 14:06:44 -0400

    feat[compoents]: Filter

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { THEME } from './src/styles/theme';
 
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
